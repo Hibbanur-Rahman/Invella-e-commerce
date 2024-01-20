29ed110f3f824902a9c6282b40ec43a49d241b72
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2024-01-20 19:52:58 +0530

    adding the home banner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import HomeBanner from'./components/homeBanner.jsx';
 function App() {
   return (
     <div className="App">
         <Navbar/>
+        <HomeBanner/>
     </div>
   );
 }
---
+++ src/components/homeBanner.jsx
@@ -0,0 +1,36 @@
+import "../assets/styles/style.css";
+import turnLeftIcon from "../assets/images/turnLeftIcon.svg";
+const HomeBanner = () => {
+  return (
+    <>
+      <div className="homeBanner row m-0 p-0 align-items-center justify-content-center ">
+        <div className="col-12">
+          <h1 className="text-light ">THE BEST SELECTION OF WATCHES BANDS</h1>
+          <button className="shopNow btn btn-lg text-light w-auto">
+            Shop Now
+          </button>
+        </div>
+      </div>
+      <div className="homeBanner-lower row m-0 p-0">
+        <div className="col-6 col-md-3 d-flex align-items-center justify-content-center">
+          <i class="bi bi-bag-fill text-light fs-2 m-0 p-0 pe-2"></i>
+          <p className="m-0 p-0 text-light fs-5 w-auto">Over 1 Million Straps Sold</p>
+        </div>
+        <div className="col-6 col-md-3 d-flex align-items-center justify-content-center">
+          <i class="bi bi-headset text-light fs-2 m-0 p-0 pe-2"></i>
+          <p className="m-0 p-0 text-light fs-5 w-auto">24/7 Customer Support</p>
+        </div>
+        <div className="col-6 col-md-3 d-flex align-items-center justify-content-center">
+          <i class="bi bi-emoji-smile text-light fs-2 m-0 p-0 pe-2"></i>
+          <p className="m-0 p-0 text-light fs-5 w-auto">100% Satisfaction</p>
+        </div>
+        <div className="col-6 col-md-3 d-flex align-items-center justify-content-center">
+          <i class="bi bi-arrow-90deg-left text-light fs-2 m-0 p-0 pe-2"></i>
+          <p className="m-0 p-0 text-light fs-5 w-auto">Easy Returns</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomeBanner;
