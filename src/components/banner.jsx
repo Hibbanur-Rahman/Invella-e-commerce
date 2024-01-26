ebca5fd8c5d1984b0214a97b2f216a6baec7dedc
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2024-01-26 10:59:22 +0530

    banner height adjust

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,7 +1,7 @@
 import "../assets/styles/style.css";
 const Banner = (props) => {
   return (
-    <div className="banner row m-0 p-0 align-items-center justify-content-center">
+    <div className="banner mt-5 mt-md-0 row m-0 p-0 align-items-center justify-content-center">
       <div className="inner-elements row m-0 p-0 align-items-center justify-content-center h-100 ">
         <div className="row m-0 p-0">
           <h2 className="text-light text-center">{props.head}</h2>
