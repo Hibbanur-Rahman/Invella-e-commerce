5580586593d394d4cb0f2dd9cf2c17cbc3c05041
Hibbanur-Rahman <hibbanrahmanhyt@gmail.com>
2024-02-27 18:16:56 +0530

    adding the add category functionality

--- src/components/adminDashboardSidebar.jsx
+++ src/components/adminDashboardSidebar.jsx
@@ -3,6 +3,15 @@ import { Link } from "react-router-dom";
 import Cookies from "js-cookie";
 import { toast } from "react-toastify";
 
+
+import dashboardIcon from '../assets/images/dashboard/dashboardIcon.svg';
+import address from '../assets/images/dashboard/address.svg';
+import downloads from '../assets/images/dashboard/downloads.svg';
+import logout from '../assets/images/dashboard/logout.svg';
+import ordersIcon from '../assets/images/dashboard/ordersIcon.svg';
+import profile from '../assets/images/dashboard/profile.svg';
+
+
 const AdminDashboardSidebar = () => {
   const handleLogout = () => {
     const token = Cookies.remove("token");
@@ -13,78 +22,95 @@ const AdminDashboardSidebar = () => {
   };
 
   return (
-    <div className="col-3 m-0 p-0">
+    <div className="col-2 m-0 p-0">
       <ul className="list-unstyled">
         <li className="pt-3 pb-3 border border-1 border-top-0 border-start-0 border-end-0">
-          <Link
+        <Link
             to="/admin/dashboard"
-            className="text-decoration-none text-secondary pt-2 pb-2"
+            className="text-decoration-none text-secondary pt-2 pb-2 justify-content-between d-flex "
           >
-            Dashboard{" "}
+           <p className="m-0 p-0 w-auto "> Dashboard</p>
+            <img src={dashboardIcon} alt="Dashboard Icon" className="w-auto"/>
           </Link>
         </li>
         <li className="pt-3 pb-3 border border-1 border-top-0 border-start-0 border-end-0">
-          <Link
+        <Link
             to="/admin/orders"
-            className="text-decoration-none text-secondary pt-2 pb-2"
+            className="text-decoration-none text-secondary pt-2 pb-2 justify-content-between d-flex"
           >
-            Orders
+            <p className="m-0 p-0 w-auto ">Orders</p>
+            <img src={ordersIcon} alt="Orders Icon" className="w-auto"/>
+
           </Link>
         </li>
         <li className="pt-3 pb-3 border border-1 border-top-0 border-start-0 border-end-0">
           <Link
             to="/admin/product"
-            className="text-decoration-none text-secondary pt-2 pb-2"
+            className="text-decoration-none text-secondary pt-2 pb-2 justify-content-between d-flex"
           >
-            Product
+            <p className="m-0 p-0 w-auto ">Product</p>
+            <img src={ordersIcon} alt="Orders Icon" className="w-auto"/>
+
           </Link>
         </li>
         <li className="pt-3 pb-3 border border-1 border-top-0 border-start-0 border-end-0">
           <Link
             to="/admin/category"
-            className="text-decoration-none text-secondary pt-2 pb-2"
-          >
-            Category
+            className="text-decoration-none text-secondary pt-2 pb-2 justify-content-between d-flex"
+            >
+              <p className="m-0 p-0 w-auto ">Category</p>
+              <img src={ordersIcon} alt="Orders Icon" className="w-auto"/>
+  
           </Link>
         </li>
         <li className="pt-3 pb-3 border border-1 border-top-0 border-start-0 border-end-0">
           <Link
             to="/admin/users-details"
-            className="text-decoration-none text-secondary pt-2 pb-2"
-          >
-            Users
+            className="text-decoration-none text-secondary pt-2 pb-2 justify-content-between d-flex"
+            >
+              <p className="m-0 p-0 w-auto ">Users</p>
+              <img src={profile} alt="profile Icon" className="w-auto"/>
+  
           </Link>
         </li>
         <li className="pt-3 pb-3 border border-1 border-top-0 border-start-0 border-end-0">
           <Link
             to="/admin/account-details"
-            className="text-decoration-none text-secondary pt-2 pb-2"
-          >
-            Account Details
+            className="text-decoration-none text-secondary pt-2 pb-2 justify-content-between d-flex"
+            >
+              <p className="m-0 p-0 w-auto ">Account Details</p>
+              <img src={profile} alt="profile Icon" className="w-auto"/>
+  
           </Link>
         </li>
         <li className="pt-3 pb-3 border border-1 border-top-0 border-start-0 border-end-0">
           <Link
             to="/admin/downloads"
-            className="text-decoration-none text-secondary pt-2 pb-2"
-          >
-            Downloads
+            className="text-decoration-none text-secondary pt-2 pb-2 justify-content-between d-flex"
+            >
+              <p className="m-0 p-0 w-auto ">Downloads</p>
+              <img src={downloads} alt="Downloads Icon" className="w-auto"/>
+  
           </Link>
         </li>
         <li className="pt-3 pb-3 border border-1 border-top-0 border-start-0 border-end-0">
           <Link
             to="/admin/address"
-            className="text-decoration-none text-secondary pt-2 pb-2"
-          >
-            Address
+            className="text-decoration-none text-secondary pt-2 pb-2 justify-content-between d-flex"
+            >
+             <p className="m-0 p-0 w-auto "> Address</p>
+             <img src={address} alt="Address Icon" className="w-auto"/>
+  
           </Link>
         </li>
         <li className="pt-3 pb-3 border border-1 border-top-0 border-start-0 border-end-0">
           <Link
             onClick={handleLogout}
-            className="text-decoration-none text-secondary pt-2 pb-2"
-          >
-            Logout
+            className="text-decoration-none text-secondary pt-2 pb-2 justify-content-between d-flex"
+            >
+              <p className="m-0 p-0 w-auto ">Logout</p>
+              <img src={logout} alt="Logout Icon" className="w-auto"/>
+  
           </Link>
         </li>
       </ul>
--- src/views/adminDashboard/category.jsx
+++ src/views/adminDashboard/category.jsx
@@ -1,8 +1,56 @@
-const Category=()=>{
+import { useState } from "react";
+import { toast } from "react-toastify";
+import axios from "axios";
+import Cookies from "js-cookie";
+const Category = () => {
+
+    const [categoryName, setCategoryName] = useState(null);
+    const handleAddCategory = async (e) => {
+        e.preventDefault();
+        try {
+            const token = Cookies.get('token');
+            const response = await axios.post('http://localhost:8000/add-category', { categoryName }, {
+                headers: {
+                    Authorization: token
+                }
+            })
+            console.log(response);
+            if (response.status === 201) {
+                toast.success('adding successfully')
+            } else {
+                toast.error("failed to adding");
+            }
+        } catch (error) {
+            toast.error("Failed to adding category")
+            console.log("error is: ", error);
+        }
+    }
+    const handleInputChange = (e) => {
+        setCategoryName(e.target.value)
+    }
     return (
-        <>
-            <h1>Category</h1>
-        </>
+        <div className="row m-0 p-0">
+            <form action="/add-category"
+                method="post"
+                onSubmit={handleAddCategory}
+            >
+                <div className="mb-3">
+                    <label htmlFor="add-category" className="form-label">
+                        Add Category
+                    </label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="addCategory"
+                        name="categoryName"
+                        value={categoryName}
+                        onChange={handleInputChange}
+                        required
+                    />
+                </div>
+                <button className="btn" onSubmit={handleAddCategory}>Submit</button>
+            </form>
+        </div>
     )
 }
 
